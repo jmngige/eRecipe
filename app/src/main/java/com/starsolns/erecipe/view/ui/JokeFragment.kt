304bc327fc8c3ba03022a9e9f71f607d7a9f3a7a
jmngige <jamesmngige@gmail.com>
2022-03-26 10:49:36 +0300

    ui design commit

--- app/src/main/java/com/starsolns/erecipe/view/ui/JokeFragment.kt
+++ app/src/main/java/com/starsolns/erecipe/view/ui/JokeFragment.kt
@@ -12,6 +12,7 @@ class JokeFragment : Fragment() {
     private var _binding: FragmentJokeBinding? = null
     private val binding get() = _binding!!
 
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
