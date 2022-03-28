4641d16512510412efbbaa7b8e19593289c94528
jmngige <jamesmngige@gmail.com>
2022-03-28 13:11:49 +0300

    Data Source commit

--- app/src/main/java/com/starsolns/erecipe/data/datasource/RemoteDataSource.kt
+++ app/src/main/java/com/starsolns/erecipe/data/datasource/RemoteDataSource.kt
@@ -8,7 +8,6 @@ import javax.inject.Inject
 class RemoteDataSource @Inject constructor(
     private val recipeApi: RecipeApi
 ) {
-
     suspend fun getRecipes(queries: Map<String, String>): Response<Recipe>{
         return recipeApi.getRecipes(queries)
     }
