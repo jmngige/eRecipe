66149f59e6f116c19d82070818ec562c731eae5f
jmngige <jamesmngige@gmail.com>
2022-03-28 13:07:52 +0300

    Network Module commit

---
+++ app/src/main/java/com/starsolns/erecipe/ERecipe.kt
@@ -0,0 +1,8 @@
+package com.starsolns.erecipe
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ERecipe: Application() {
+}
---
+++ app/src/main/java/com/starsolns/erecipe/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.starsolns.erecipe.di
+
+import com.starsolns.erecipe.data.retrofit.RecipeApi
+import com.starsolns.erecipe.util.Constants.Companion.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): RecipeApi{
+        return retrofit.create(RecipeApi::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConverterFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConverterFactory)
+            .build()
+    }
+
+
+    @Singleton
+    @Provides
+    fun provideOkHttpCLient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .callTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGsonConverterFactory(): GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+}
