83ad26097a8c9601903a366867d8a3e5206f065d
jmngige <jamesmngige@gmail.com>
2022-03-28 13:10:18 +0300

    Network Module commit

---
+++ app/src/main/java/com/starsolns/erecipe/data/datasource/RemoteDataSource.kt
@@ -0,0 +1,16 @@
+package com.starsolns.erecipe.data.datasource
+
+import com.starsolns.erecipe.data.retrofit.RecipeApi
+import com.starsolns.erecipe.model.Recipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val recipeApi: RecipeApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<Recipe>{
+        return recipeApi.getRecipes(queries)
+    }
+
+}
