d29e49d90e4b6b73e21a71bfe8834b46684cc647
jmngige <jamesmngige@gmail.com>
2022-03-31 10:33:48 +0300

    Database Caching commit

--- app/src/main/java/com/starsolns/erecipe/di/DatabaseModule.kt
+++ app/src/main/java/com/starsolns/erecipe/di/DatabaseModule.kt
@@ -5,12 +5,15 @@ import androidx.room.Room
 import com.starsolns.erecipe.data.room.RecipeDatabase
 import com.starsolns.erecipe.util.Constants.Companion.DATABASE_NAME
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @HiltAndroidApp
-class DatabaseModule {
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
 
     @Singleton
     @Provides
