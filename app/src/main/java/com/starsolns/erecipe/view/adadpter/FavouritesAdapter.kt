cff6890121426ad181b531c8ee7704e0f141bc0a
jmngige <jamesmngige@gmail.com>
2022-04-21 07:19:49 +0300

    Details Activity Impl

--- app/src/main/java/com/starsolns/erecipe/view/adadpter/FavouritesAdapter.kt
+++ app/src/main/java/com/starsolns/erecipe/view/adadpter/FavouritesAdapter.kt
@@ -3,10 +3,12 @@ package com.starsolns.erecipe.view.adadpter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.starsolns.erecipe.data.room.entities.FavouriteEntity
 import com.starsolns.erecipe.databinding.FavouritesItemLayoutBinding
+import com.starsolns.erecipe.view.ui.FavouritesFragmentDirections
 
 class FavouritesAdapter(private val context: Context): RecyclerView.Adapter<FavouritesAdapter.ViewHolder>() {
 
@@ -20,6 +22,10 @@ class FavouritesAdapter(private val context: Context): RecyclerView.Adapter<Favo
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val currentFavourite = favouritesList[position]
         holder.bind(currentFavourite)
+        holder.itemViewLayout.setOnClickListener {
+            val action = FavouritesFragmentDirections.actionFavouritesFragmentToDetailsActivity(currentFavourite.result)
+            holder.itemViewLayout.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int {
@@ -34,6 +40,8 @@ class FavouritesAdapter(private val context: Context): RecyclerView.Adapter<Favo
     }
 
     inner class ViewHolder(private val binding: FavouritesItemLayoutBinding): RecyclerView.ViewHolder(binding.root){
+
+        val itemViewLayout = binding.favouritesItemLayout
 
         fun bind(favourite: FavouriteEntity){
            binding.recipe = favourite
