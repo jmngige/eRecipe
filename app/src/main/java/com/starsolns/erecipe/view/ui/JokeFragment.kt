6dc20bf1dbca17c7a2ad20050debdf0cdadbaf9a
jmngige <jamesmngige@gmail.com>
2022-04-22 16:05:49 +0300

    Jokes Api Impl

--- app/src/main/java/com/starsolns/erecipe/view/ui/JokeFragment.kt
+++ app/src/main/java/com/starsolns/erecipe/view/ui/JokeFragment.kt
@@ -1,15 +1,15 @@
 package com.starsolns.erecipe.view.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.util.Log
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import com.starsolns.erecipe.R
 import com.starsolns.erecipe.databinding.FragmentJokeBinding
 import com.starsolns.erecipe.util.Constants.Companion.API_KEY
 import com.starsolns.erecipe.util.NetworkResult
@@ -23,7 +23,7 @@ class JokeFragment : Fragment() {
     private val binding get() = _binding!!
 
     private lateinit var mainViewModel: MainViewModel
-
+    private  var foodJoke: String = "No Joke Found"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -40,6 +40,7 @@ class JokeFragment : Fragment() {
                     binding.progressBar.visibility = View.INVISIBLE
                     binding.materialCardView.visibility = View.VISIBLE
                     binding.foodJokeText.text = response.data!!.text
+                    foodJoke = response.data!!.text
                 }
                 is NetworkResult.Error ->{
 
@@ -67,7 +68,23 @@ class JokeFragment : Fragment() {
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.food_joke_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share_joke -> {
+                val shareIntent = Intent().apply {
+                    this.action = Intent.ACTION_SEND
+                    this.type = "text/plain"
+                    this.putExtra(Intent.EXTRA_TEXT, foodJoke)
+                }
+                startActivity(Intent.createChooser(shareIntent, "Share with"))
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     override fun onDestroyView() {
         super.onDestroyView()
